c3e9242e8c2608a2ad48caa91a507746fbb60b16
KOLT77 <RoLaN100@yandex.ru>
2017-06-04 16:24:53 +0700

    Update register.php

--- register.php
+++ register.php
@@ -9,28 +9,28 @@
                <div id="basket" style="background:none;">
                   <?php 
                      if (isset($_SESSION['login'])) {
-							echo '<script>location.href="index.php"</script>';
-						   }
-					      else {
-                        $login=htmlspecialchars($_POST['login']);
-                        $password=htmlspecialchars($_POST['password']);
-                        $logins=file('files/logins3Fd2D093F23d5FQ2O2.txt', FILE_IGNORE_NEW_LINES);
+				echo '<script>location.href="index.php"</script>';
+			}
+			else {
+				$login=htmlspecialchars($_POST['login']);
+				$password=htmlspecialchars($_POST['password']);
+				$logins=file('files/logins3Fd2D093F23d5FQ2O2.txt', FILE_IGNORE_NEW_LINES);
 
-                        for ($i=0; $i<count($logins); $i++)
-                        {
-                           if ($login==$logins[$i])
-                           {
-                              $_SESSION['login_already_exist']=true;
-                              echo '<script>location.href="registration.php"</script>';
-                              exit;
-                           }
-                        }
+				for ($i=0; $i<count($logins); $i++)
+				{
+				   if ($login==$logins[$i])
+				   {
+				      $_SESSION['login_already_exist']=true;
+				      echo '<script>location.href="registration.php"</script>';
+				      exit;
+				   }
+				}
 
-                        file_put_contents('files/logins3Fd2D093F23d5FQ2O2.txt', $login."\n", FILE_APPEND);
-                        file_put_contents('files/passwords9Y79Hqyd83JNBX8Q12.txt', $password."\n", FILE_APPEND);
-                        $_SESSION['login']=$login;
-                        echo '<p align="center" style="font-size:24px;color:#000;font-weight:bold;"> Вы успешно зарегистрированы!</p>';
-                     }      
+				file_put_contents('files/logins3Fd2D093F23d5FQ2O2.txt', $login."\n", FILE_APPEND);
+				file_put_contents('files/passwords9Y79Hqyd83JNBX8Q12.txt', $password."\n", FILE_APPEND);
+				$_SESSION['login']=$login;
+				echo '<p align="center" style="font-size:28px;color:#000;font-weight:bold;"> Вы успешно зарегистрированы!</p>';
+			     }      
                   ?>
                </div>
             </div>
